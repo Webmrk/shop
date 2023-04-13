49035859e00c69e3fb89bffe5e84cc105d16d34e
mirko <web@mrksolution.it>
2023-04-13 10:41:01 +0200

    primo commit

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+const Layout = () => {
+  console.log("ciao");
+
+  return (
+    <div>
+      <header>header</header>
+      <div>content</div>
+    </div>
+  );
+};
+
+export default Layout;
